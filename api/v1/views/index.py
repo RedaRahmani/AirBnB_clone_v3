8454b12991565004d2cc32f58393c26b73667717
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-10-30 12:54:57 -0700

    add task04

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 """
-index content view for API.
+index .
 """
 
 from flask import jsonify
 import models
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 @app_views.route("/status", strict_slashes=False)
 def view_status():
     """
-    status okay for /status
+    status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def states():
+    """
+    Create an endpoint states
+    """
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
