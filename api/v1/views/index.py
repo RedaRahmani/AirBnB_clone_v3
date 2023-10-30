3009f9e58bccfd133b6c33b706c263ace5d98d3a
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-10-30 12:50:19 -0700

    fix task03

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
+"""
+index content view for API.
+"""
+
 from flask import jsonify
+import models
+from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def view_status():
     """
-    status
+    status okay for /status
     """
     return jsonify({"status": "OK"})
